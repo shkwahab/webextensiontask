9eed2b3224d4c1e0a53b6a787ba3b2370076f6cc
shkwahab <wahab209266@gmail.com>
2024-08-05 23:32:31 +0500

    basic popup extension

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Popup from './Popup'
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Popup/>
+    </React.Fragment>
+  )
+}
+
+export default App
---
+++ src/Popup.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Popup() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>This is my web extension popup.</p>
+    </div>
+  );
+}
+
+export default Popup;
